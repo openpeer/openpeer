0f40bd556ff38d895a2e59f8d712b63d439bda0a
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-12-07 16:31:19 +0000

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import './globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      {/*
+      <div>
+        {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
+        <head />
+        <body>
+          <div className="container mx-auto">{children}</div>
+        </body>
+      </div>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,131 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import Image, { StaticImageData } from 'next/image';
+import { useState } from 'react';
+
+import { Manrope } from '@next/font/google';
+
+import arrows from '../public/arrows.svg';
+import lock from '../public/lock.png';
+import logo from '../public/logo.svg';
+import passport from '../public/passport.png';
+import people from '../public/people.png';
+import styles from './page.module.css';
+
+const regular = Manrope({
+  subsets: ['latin'],
+  weight: '400'
+});
+const semibold = Manrope({
+  subsets: ['latin'],
+  weight: '600'
+});
+const bold = Manrope({
+  subsets: ['latin'],
+  weight: '800'
+});
+
+interface FeatureParams {
+  title: string;
+  description: string;
+  image: StaticImageData;
+}
+
+const Feature = ({ title, description, image }: FeatureParams) => {
+  return (
+    <div className="flex flex-col items-center mb-12">
+      <Image src={image} alt={title} width={176} className="mb-4" />
+      <h3 className="text-2xl mb-4">{title}</h3>
+      <div className={regular.className}>
+        <p className="text-center text-base">{description}</p>
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
+  const [navbar, setNavbar] = useState(false);
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
+    <div>
+      <nav className="mt-10 relative  flex justify-between items-center w-full">
+        <a className="text-3xl font-bold leading-none" href="#">
+          <Image src={logo} alt="openpeer logo" width={234} height={51} />
+        </a>
+        <div className="sm:hidden" onClick={() => setNavbar(!navbar)}>
+          <button className="navbar-burger flex items-center p-3">
+            <svg
+              className="block h-4 w-4 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Mobile menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+            </svg>
+          </button>
+        </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+        <div className="flex flex-row justify-between space-x-8 text-xl hidden sm:block">
+          <a href="faq.html">FAQ</a>
+          <a href="docs.html">Docs</a>
+        </div>
+      </nav>
 
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
+      <div className={`navbar-menu ${navbar ? '' : 'hidden'}`}>
+        <div className="flex flex-column space-x-8 text-xl">
+          <ul className="text-center flex-1">
+            <li>
+              <a href="faq.html">FAQ</a>
+            </li>
+            <li>
+              <a href="docs.html">Docs</a>
+            </li>
+          </ul>
+        </div>
+      </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
+      <div className="mt-20 sm:mt-40 mb-20">
+        <div className="text-center text-5xl sm:text-6xl mb-8">
+          <div className={semibold.className}>
+            <h1>Decentralized P2P</h1>
+          </div>
+        </div>
+        <div className={regular.className}>
+          <div className="flex flex-row justify-center space-x-4 text-3xl mb-12">
+            <h2>crypto</h2>
+            <Image src={arrows} alt="swap arrows" />
+            <h2>fiat</h2>
+          </div>
+        </div>
+        <div className="text-center">
+          <a href="https://minke.app" className="bg-[#222222] px-20 py-4 rounded-lg">
+            Launch App
           </a>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      </div>
+      <div className="flex flex-col justify-around mx-20 md:space-x-20 md:flex-row">
+        {[
+          {
+            title: 'Self-custodial',
+            description:
+              'Only you control your crypto. Deploy a smart contract with your funds that can only be withdrawn by the other party upon payment.',
+            image: lock
+          },
+          {
+            title: 'No KYC',
+            description:
+              'No KYC necessary. Funds are never custodied by any central party. Earn reputation for every successful transaction.',
+            image: passport
+          },
+          {
+            title: 'Community Moderated',
+            description:
+              'Disputes are moderated by a decentralized autonomous organization (DAO) of local experts who review evidence and vote on an outcome.',
+            image: people
+          }
+        ].map((item) => (
+          <Feature key={item.title} {...item} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
