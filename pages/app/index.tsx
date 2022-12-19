f78d675c1af91a6feae2976f5df81748fc62fcc5
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-12-19 12:14:27 +0000

    Add merchant type

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,7 +1,7 @@
-import { Key, ReactElement, JSXElementConstructor, ReactFragment } from 'react';
 import Button from '../../components/Button/Button';
+import Merchant from '../../types/merchant';
 
-const merchant = [
+const merchant: Merchant[] = [
   {
     id: 1,
     name: 'Josh Reyes',
@@ -9,8 +9,7 @@ const merchant = [
     amount: '₹520',
     avaliable: '0.02394BTC',
     limit: '₹ 200-₹300',
-    price: '₹520',
-    isCurrent: false,
+    price: '₹520'
   },
   {
     id: 2,
@@ -19,8 +18,7 @@ const merchant = [
     amount: '₹720',
     avaliable: '0.02394BTC',
     limit: '₹ 200-₹300',
-    price: '₹520',
-    isCurrent: false,
+    price: '₹520'
   },
   {
     id: 3,
@@ -29,78 +27,79 @@ const merchant = [
     amount: '₹20',
     avaliable: '0.02394BTC',
     limit: '₹ 100-₹200',
-    price: '₹520',
-    isCurrent: false,
-  },
-]
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
-}
+    price: '₹520'
+  }
+];
 
 export default function HomePage() {
   return (
     <div className="py-6">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-        <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
-      </div>
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
         {/* Replace with your content */}
         <div className="py-4">
-          
-
           {/* Table structure START  */}
-          <table className="min-w-full md:rounded-lg overflow-hidden" style={{ borderSpacing: 0 }}>
+          <table className="min-w-full md:rounded-lg overflow-hidden border-spacing-0">
             <thead className="bg-gray-100">
               <tr>
-                <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                <th
+                  scope="col"
+                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                >
                   Merchant
                 </th>
-                <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                >
                   Volume
                 </th>
-                <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                >
                   Amount
                 </th>
-                <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                >
                   Limit
                 </th>
-                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
                   &nbsp;
                 </th>
               </tr>
             </thead>
             <tbody className="divide-y divide-gray-200 bg-white">
-              {merchant.map((merchant: { id: Key | null | undefined; name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | null | undefined; isCurrent: boolean | undefined; volume: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | null | undefined; amount: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | null | undefined; limit: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | null | undefined; price: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | null | undefined; }, merchantIdx: number) => (
-                <tr key={merchant.id} className="hover:bg-gray-50">
+              {merchant.map(({ id, name, volume, amount, limit }) => (
+                <tr key={id} className="hover:bg-gray-50">
                   <td className="pl-4 py-4">
-                    <div className="text-sm text-gray-900">
-                      {merchant.name}
-                    </div>
+                    <div className="text-sm text-gray-900">{name}</div>
                     <div className="mt-1 flex flex-col text-gray-500 sm:block lg:hidden">
-                      <span>Volume: {merchant.volume}</span>
-                      <span>Amount: {merchant.amount}</span>
+                      <span>Volume: {volume}</span>
+                      <span>Amount: {amount}</span>
                     </div>
                   </td>
                   <td className="hidden px-3.5 py-3.5 text-sm text-gray-500 lg:table-cell">
-                    {merchant.volume}
+                    {volume}
                   </td>
                   <td className="hidden px-3.5 py-3.5 text-sm text-gray-500 lg:table-cell">
-                    {merchant.amount}
+                    {amount}
                   </td>
                   <td className="hidden px-3.5 py-3.5 text-sm text-gray-500 lg:table-cell">
-                    {merchant.limit}
+                    {limit}
                   </td>
                   <td className="text-right py-4 pr-4">
-                    <Button title="Buy" /> 
+                    <Button title="Buy" />
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
           {/* Table structure END  */}
-
-
         </div>
         {/* /End replace */}
       </div>
---
+++ types/merchant.ts
@@ -0,0 +1,9 @@
+export default interface Merchant {
+  id: number;
+  name: string;
+  volume: string;
+  amount: string;
+  limit: string;
+  price: string;
+  avaliable: string;
+}
