6605b995f8acebd3036a170401426826717fb955
avelar <fortunato.avelar@gmail.com>
2022-12-22 10:26:42 -0300

    refactor crypto listing select order type screen

---
+++ pages/app/listing.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { useState } from 'react';
+
+import Button from '../../components/Button/Button';
+import List from '../../components/List';
+
+const SellPage = () => {
+  const [step, setStep] = useState(1);
+
+  return (
+    <div className="py-6">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 mb-16">
+        <h1 className="text-2xl font-semibold text-gray-900">Choose order type</h1>
+      </div>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+        
+        <List />
+        <Link href="/app/sell">
+          <Button title="Proceed" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SellPage;
--- pages/app/sell/index.tsx
+++ pages/app/sell/index.tsx
@@ -1,23 +1,45 @@
-import Link from 'next/link';
 import { useState } from 'react';
 
 import Button from '../../../components/Button/Button';
-import List from '../../../components/List';
+import CurrencySelect from '../../../components/Select/CurrencySelect';
+import TokenSelect from '../../../components/Select/TokenSelect';
+import Steps from '../../../components/Steps';
 
 const SellPage = () => {
   const [step, setStep] = useState(1);
+  const proceed = () => {
+    setStep(step + 1);
+  };
 
   return (
     <div className="py-6">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 mb-16">
-        <h1 className="text-2xl font-semibold text-gray-900">Choose order type</h1>
+        <h1 className="text-2xl font-semibold text-gray-900">Crypto Listing</h1>
       </div>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-        
-        <List />
-        <Link href="/app/sell/token">
-          <Button title="Proceed" />
-        </Link>
+        <Steps currentStep={step} />
+        {step === 1 && (
+          <>
+            <TokenSelect />
+            <CurrencySelect />
+          </>
+        )}
+        {step === 2 && (
+          <>
+            <h1>2</h1>
+          </>
+        )}
+        {step === 3 && (
+          <>
+            <h1>3</h1>
+          </>
+        )}
+        {step === 4 && (
+          <>
+            <h1>4</h1>
+          </>
+        )}
+        <Button title="Proceed" onClick={proceed} />
       </div>
     </div>
   );
