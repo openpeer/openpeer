f1c53ed3ff77dcd7363b661a19ecf8aa83be1cc4
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-12-26 10:59:55 +0000

    Remove ButtonNoStyle component

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,17 +1,22 @@
 interface ButtonProps {
-  title: string
-  onClick?: () => void
+  title: string;
+  onClick?: () => void;
+  minimal?: boolean;
 }
 
-const Button = ({ title, onClick }: ButtonProps) => {
+const Button = ({ title, onClick, minimal = false }: ButtonProps) => {
   return (
     <button
-      className="w-full md:w-auto px-5 py-2.5 rounded bg-[#3C9AAA] text-base text-white"
+      className={
+        minimal
+          ? 'text-xl font-bold w-8'
+          : 'w-full md:w-auto px-5 py-2.5 rounded bg-[#3C9AAA] text-base text-white'
+      }
       onClick={onClick}
     >
       {title}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- app/components/Selector.tsx
+++ app/components/Selector.tsx
@@ -1,21 +1,21 @@
-import ButtonNoStyle from "./Button/ButtonNoStyle"
+import Button from './Button/Button';
 
 interface SelectorProps {
-  value: string
-  underValue?: string
+  value: string;
+  underValue?: string;
 }
 
 const Selector = ({ value, underValue }: SelectorProps) => {
   return (
     <div className="flex flex-row justify-between content-center bg-gray-100 my-8 py-4 p-8 border-2 border-slate-200 rounded-md">
-      <ButtonNoStyle title="-" className="text-xl font-bold w-8" />
+      <Button title="-" minimal />
       <div className="flex flex-col item-center">
         <div className="text-xl font-bold">{value}</div>
         <div className="text-sm">{underValue}</div>
       </div>
-      <ButtonNoStyle title="+" className="text-xl font-bold w-8" />
+      <Button title="+" minimal />
     </div>
-  )
-}
+  );
+};
 
-export default Selector
+export default Selector;
