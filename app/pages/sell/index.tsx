f0e925eea95a324d07d402925e6004666389f510
avelar <fortunato.avelar@gmail.com>
2022-12-29 16:45:03 -0300

    update Connect to Polygon blank state window

--- app/pages/sell/index.tsx
+++ app/pages/sell/index.tsx
@@ -1,3 +1,4 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit"
 import { Checkbox, Label, Selector, Steps, Textarea } from "components"
 import { Amount, Setup } from "components/Listing"
 import { UIList } from "components/Listing/Listing.types"
@@ -19,16 +20,30 @@ const SellPage = () => {
   const step = list.step
 
   if (!address || !chain || chain.unsupported) {
-    return <p>Connect to Polygon</p>
+    return (
+      <div className="flex h-screen">
+        <div className="m-auto flex flex-col justify-items-center content-center text-center">
+          <span className="mb-6 text-xl">Connect to Polygon</span>{" "}
+          <span className="mb-6 text-gray-500 text-xl">
+            Access the OpenPeer using your favorite wallet
+          </span>
+          <span className="mb-4 m-auto">
+            <ConnectButton showBalance={false} />
+          </span>
+        </div>
+      </div>
+    )
   }
 
   return (
     <div className="py-6">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 mb-16">
-        <h1 className="text-2xl font-semibold text-gray-900">Crypto Listing - Sell</h1>
+        <h1 className="text-2xl font-semibold text-gray-900">
+          Crypto Listing » Sell Order
+        </h1>
       </div>
       <div className="w-full flex flex-row px-4 sm:px-6 md:px-8 mb-16">
-        <div className="w-full md:w-2/4">
+        <div className="w-full lg:w-2/4">
           <Steps
             currentStep={step}
             onStepClick={(n) => setList({ ...list, ...{ step: n } })}
