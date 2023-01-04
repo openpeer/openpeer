b44dbff1c111695cf8643f71be9ea0c9834907eb
avelar <fortunato.avelar@gmail.com>
2023-01-04 10:00:34 -0300

    remove user status

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,7 +1,6 @@
 interface AvatarProps {
   size: string
   image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-  status: boolean
 }
 
 const Avatar = ({ size, image, status }: AvatarProps) => {
@@ -11,11 +10,6 @@ const Avatar = ({ size, image, status }: AvatarProps) => {
         <img className="h-8 w-8 rounded-full" src={image} />
       ) : (
         <img className="h-16 w-16 rounded-full" src={image} />
-      )}
-      {status ? (
-        <span className="absolute top-0 right-0 block h-1.5 w-1.5 rounded-full bg-green-400 ring-2 ring-white" />
-      ) : (
-        <span className="absolute top-0 right-0 block h-1.5 w-1.5 rounded-full bg-gray-300 ring-2 ring-white" />
       )}
     </span>
   )
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -87,7 +87,6 @@ const HomePage = () => {
                               image={
                                 "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                               }
-                              status={false}
                             />
                             <div className="ml-2">Crypto Lurd</div>
                           </div>
