dbf70763433136d9caae86b83ad6cc185377781f
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-01-09 12:30:30 +0000

    Missing files

---
+++ app/components/Buy/index.ts
@@ -0,0 +1,7 @@
+import Amount from './Amount';
+import Completed from './Completed';
+import Payment from './Payment';
+import Release from './Release';
+import Summary from './Summary';
+
+export { Amount, Completed, Payment, Release, Summary };
