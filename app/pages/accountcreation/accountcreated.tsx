9bb202634e203967bb11ec21d223d6b7efdbd926
avelar <fortunato.avelar@gmail.com>
2023-01-18 07:49:55 -0300

    improve content

--- app/pages/accountcreation/accountcreated.tsx
+++ app/pages/accountcreation/accountcreated.tsx
@@ -1,4 +1,4 @@
-import { CheckBadgeIcon } from '@heroicons/react/24/outline';
+import { CheckCircleIcon } from '@heroicons/react/24/outline';
 import { Button } from 'components';
 import Link from 'next/link';
 
@@ -7,13 +7,15 @@ const AccountCreated = () => {
 		<>
 			<div className="p-4">
 				<div className="flex flex-col w-full mt-8 md:w-1/2 md:m-auto md:mt-8 text-center justify-center items-center rounded-lg shadow overflow-hidden bg-white p-8">
-					<CheckBadgeIcon className="w-24 text-[#3C9AAA] m-auto mb-8" />
-					<span className="block text-2xl mb-8 text-[#3C9AAA] font-bold">Account Created</span>
-					<span className="text-lg mb-8">
-						Welcome to OpenPeer, the easy away to Trade crypto, decentralized!
+					<CheckCircleIcon className="w-24 text-[#3C9AAA] m-auto mb-2" />
+					<span className="block text-xl mb-8 text-[#3C9AAA] font-bold">Account Created</span>
+					<span className="text-xl mb-8">
+						Welcome to OpenPeer,
+						<br />
+						the decentralized way to buy and sell crypto.
 					</span>
-					<Link href="/" className="w-1/2">
-						<Button title="Go to Dashboard" />
+					<Link href="/" className="w-1/2 mb-4">
+						<Button title="Go to dashboard" />
 					</Link>
 				</div>
 			</div>
