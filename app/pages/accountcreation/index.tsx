2c972bc0dd62cc8bb51599d8c226e55ce8185203
avelar <fortunato.avelar@gmail.com>
2023-01-19 07:51:18 -0300

    improvements

--- app/pages/accountcreation/index.tsx
+++ app/pages/accountcreation/index.tsx
@@ -1,8 +1,10 @@
 import { InformationCircleIcon } from '@heroicons/react/24/outline';
 import { Button, Checkbox, Input, Textarea } from 'components';
 import Link from 'next/link';
+import React from 'react';
 
 const AccountCreation = () => {
+	const [visible, setVisible] = React.useState(false);
 	return (
 		<>
 			<div className="p-4">
@@ -11,41 +13,49 @@ const AccountCreation = () => {
 					<div className="py-4 px-8 bg-white">
 						<Input id={''} value={undefined} label="Wallet Address" />
 
-						<Input id={''} value={undefined} label="Username" addOn="10/20" />
-
-						<Input id={''} value={undefined} label="Email Address" />
-
-						<div className="flex flex-row items-center text-[#3C9AAA] text-base -ml-1">
-							<InformationCircleIcon className="w-6" />
-							<span className="pl-2">No account found for this address</span>
+						<div className={visible ? 'hidden' : 'block'}>
+							<div className="flex flex-row items-center text-[#3C9AAA] text-base -ml-1">
+								<InformationCircleIcon className="w-6" />
+								<span className="pl-2">No account found for this address</span>
+							</div>
 						</div>
 
-						<Checkbox
-							content={
-								<>
-									<span>I agree to </span>
-									<a href="/terms" className="text-[#3C9AAA] underline" target="_blank">
-										privacy policy & terms
-									</a>
-								</>
-							}
-							id="terms"
-							name="terms"
-						/>
+						{visible && (
+							<div>
+								<Input id={''} value={undefined} label="Username" addOn="10/20" />
 
-						<Checkbox content="Subscribe to our newsletter" id="newsletter" name="newsletter" />
+								<Input id={''} value={undefined} label="Email Address" />
+
+								<Checkbox
+									content={
+										<>
+											<span>I agree to </span>
+											<a href="/terms" className="text-[#3C9AAA] underline" target="_blank">
+												privacy policy & terms
+											</a>
+										</>
+									}
+									id="terms"
+									name="terms"
+								/>
+
+								<Checkbox content="Subscribe to our newsletter" id="newsletter" name="newsletter" />
+							</div>
+						)}
 
 						<div className="flex flex-col flex-col-reverse md:flex-row items-center justify-between mt-8 md:mt-0">
 							<span className="w-full md:w-1/2 md:pr-8">
 								<Button title="Switch wallet" outlined />
 							</span>
 							<span className="w-full">
-								<div className="hidden">
-									<Button title="Continue" />
+								<div className={visible ? 'hidden' : 'block'}>
+									<Button title="Continue" onClick={() => setVisible(!visible)} />
 								</div>
-								<Link href="/accountcreation/accountcreated">
-									<Button title="Create New Account" />
-								</Link>
+								<div className={visible ? 'block' : 'hidden'}>
+									<Link href="/accountcreation/accountcreated">
+										<Button title="Create New Account" />
+									</Link>
+								</div>
 							</span>
 						</div>
 					</div>
