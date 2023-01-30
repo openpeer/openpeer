071344c348600c4a12fce3ea82a847f360658078
avelar <fortunato.avelar@gmail.com>
2023-01-30 07:29:31 -0300

    create new components

---
+++ app/components/DisputeTrade/claim.tsx
@@ -0,0 +1,39 @@
+import { CloudArrowUpIcon } from '@heroicons/react/24/outline';
+import Button from 'components/Button/Button';
+import Input from 'components/Input/Input';
+import Label from 'components/Label/Label';
+import Textarea from 'components/Textarea/Textarea';
+
+const DisputeClaim = () => {
+	return (
+		<>
+			<div>
+				<Label title="Comments" />
+				<Textarea rows={0} id={''} value={undefined} placeholder="Tell us more about the transaction" />
+			</div>
+			<div>
+				<Label title="Upload proof" />
+				<label htmlFor="file-upload" className="bg-transparent relative cursor-pointer rounded-md font-medium">
+					<div className="flex flex-col items-center justify-center p-4 rounded-lg border-2 border-dashed border-cyan-600 bg-gray-100">
+						<CloudArrowUpIcon className="text-cyan-600 w-10" />
+						<div className="flex text-sm text-gray-600">
+							<span className="text-cyan-600 underline hover:no-underline">Upload a file</span>
+							<input id="file-upload" name="file-upload" type="file" className="sr-only" />
+							<p className="pl-1">or drag and drop</p>
+						</div>
+						<p className="text-xs text-gray-500 py-2">Supported formats: JPEG, JPG, PNG, PDF</p>
+					</div>
+				</label>
+			</div>
+			<div className="-mb-8">
+				<Input label="Pay" id={''} LabelSideInfo="Balance: 250USDT" />
+			</div>
+			<div className="flex flex-col md:flex-row gap-x-8 items-center">
+				<Button title="Cancel" outlined />
+				<Button title="Continue" />
+			</div>
+		</>
+	);
+};
+
+export default DisputeClaim;
---
+++ app/components/DisputeTrade/status.tsx
@@ -0,0 +1,14 @@
+const DisputeStatus = () => {
+	return (
+		<div>
+			<div className="flex flex-row justify-between border-b pb-4">
+				<div className="font-bold">Dispute Pending</div>
+				<div className="text-cyan-600">
+					Time left <span>15m:20secs</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default DisputeStatus;
