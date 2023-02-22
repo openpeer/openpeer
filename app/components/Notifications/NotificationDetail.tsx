767925c11bd4656ff47d0c3a69d2f75f30754b52
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-02-22 11:39:43 +0000

    Fix notifications

--- app/components/Notifications/NotificationDetail.tsx
+++ app/components/Notifications/NotificationDetail.tsx
@@ -2,10 +2,10 @@ import { NotificationParams } from 'models/notification';
 import { useRouter } from 'next/router';
 
 import { XMarkIcon } from '@heroicons/react/20/solid';
-import { InboxIcon } from '@heroicons/react/24/outline';
+import { CheckCircleIcon, InformationCircleIcon } from '@heroicons/react/24/outline';
 
 const NotificationDetail = ({ notification, onOpen, onDismiss }: NotificationParams) => {
-	const { id, title, description, url } = notification;
+	const { id, title, description, url, unread } = notification;
 	const router = useRouter();
 	const onClick = () => {
 		onOpen(id);
@@ -14,15 +14,24 @@ const NotificationDetail = ({ notification, onOpen, onDismiss }: NotificationPar
 		}
 	};
 
+	const onDismissClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.stopPropagation();
+		onDismiss(id);
+	};
+
 	return (
 		<div className="p-4" onClick={onClick}>
 			<div className="flex items-start">
 				<div className="flex-shrink-0">
-					<InboxIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
+					{unread ? (
+						<InformationCircleIcon className="h-6 w-6 text-red-400" aria-hidden="true" />
+					) : (
+						<CheckCircleIcon className="h-6 w-6 text-[#3C9AAA]" aria-hidden="true" />
+					)}
 				</div>
 				<div className="ml-3 w-0 flex-1 pt-0.5">
 					<p className="text-left text-sm font-medium text-gray-900">{title}</p>
-					<p className="mt-1 text-sm text-gray-500" dangerouslySetInnerHTML={{ __html: description! }}></p>
+					<p className="text-sm text-gray-500" dangerouslySetInnerHTML={{ __html: description! }}></p>
 					<div className="mt-3 flex space-x-7">
 						<button
 							type="button"
@@ -31,20 +40,13 @@ const NotificationDetail = ({ notification, onOpen, onDismiss }: NotificationPar
 						>
 							See details
 						</button>
-						<button
-							type="button"
-							className="rounded-md bg-white text-sm font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-							onClick={() => onDismiss(id)}
-						>
-							Dismiss
-						</button>
 					</div>
 				</div>
 				<div className="ml-4 flex flex-shrink-0">
 					<button
 						type="button"
 						className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-						onClick={() => onDismiss(id)}
+						onClick={onDismissClick}
 					>
 						<span className="sr-only">Close</span>
 						<XMarkIcon className="h-5 w-5" aria-hidden="true" />
