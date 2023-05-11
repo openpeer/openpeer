2b3d9f617b231e787a498d05283dc4ee39a72c71
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-05-11 12:23:31 +0100

    Update logo

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 
 import Image from 'next/image';
 
+import discord from '../public/discord.svg';
 import logo from '../public/logo.svg';
 import twitterLogo from '../public/twitter.svg';
 
@@ -18,9 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 							<a className="text-3xl font-bold leading-none" href="#">
 								<Image src={logo} alt="openpeer logo" width={132} height={51} />
 							</a>
-							<p className="w-80 mt-8">
-								OpenPeer is a completely decentralized P2P on/off ramp for emerging markets.
-							</p>
+							<p className="w-80 mt-8">OpenPeer is a decentralized P2P exchange protocol.</p>
 						</div>
 						<div className="flex mt-8 content-center md:mt-0 md:self-start justify-center md:justify-start">
 							<div className="flex flex-row items-center">
@@ -40,6 +39,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 									className="px-2"
 								>
 									<Image src={twitterLogo} alt="openpeer logo" width={20} height={20} />
+								</a>
+								<a
+									href="https://discord.gg/Wrf9BT8sZN"
+									target="_blank"
+									rel="noreferrer"
+									className="px-2"
+								>
+									<Image src={discord} alt="openpeer logo" width={20} height={20} />
 								</a>
 							</div>
 						</div>
