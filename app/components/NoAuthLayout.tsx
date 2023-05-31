4242e8819d731be266bc0f79bac827ffe8fc7b87
avelar <fortunato.avelar@gmail.com>
2023-05-30 23:40:54 -0300

    refactor dropdown menu code

--- app/components/NoAuthLayout.tsx
+++ app/components/NoAuthLayout.tsx
@@ -26,6 +26,12 @@ const navigation = [
 	{ name: 'Post Ad', href: '/sell', icon: PlusCircleIcon },
 	{ name: 'My Trades', href: '/orders', icon: ShoppingBagIcon }
 ];
+const navigationItem = [
+	{ name: 'Docs', href: 'https://docs.openpeer.xyz/' },
+	{ name: 'Disclamer', href: 'https://openpeer.xyz/disclamer' },
+	{ name: 'Terms', href: 'https://openpeer.xyz/terms' },
+	{ name: 'Privacy Policy', href: 'https://openpeer.xyz/privacy-policy' }
+];
 
 const NoAuthLayout = ({ Component, pageProps }: AppProps) => {
 	const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -77,15 +83,10 @@ const NoAuthLayout = ({ Component, pageProps }: AppProps) => {
 										</Link>
 									))}
 									{/* dropdown menu START */}
-									<div className="relative" data-headlessui-state="open">
+									<div className="relative">
 										<button
 											className="flex items-center font-semibold hover:text-gray-900"
-											id="headlessui-menu-button-1"
 											type="button"
-											aria-haspopup="menu"
-											aria-expanded="true"
-											data-headlessui-state="open"
-											aria-controls="headlessui-menu-items-122"
 											onClick={toggleDiv}
 										>
 											<span className="hidden items-center sm:flex text-gray-500">•••</span>
@@ -94,50 +95,24 @@ const NoAuthLayout = ({ Component, pageProps }: AppProps) => {
 											className={`absolute right-0 top-full -mr-0.5 mt-3 w-60 origin-top-right divide-y divide-gray-100 rounded-lg bg-white text-sm font-normal text-slate-900 shadow-md ring-1 ring-slate-900/5 focus:outline-none sm:-mr-3.5 transform opacity-100 scale-100 z-50 ${
 												isDivVisible ? '' : 'hidden'
 											}`}
-											// className="absolute right-0 top-full -mr-0.5 mt-3 w-60 origin-top-right divide-y divide-gray-100 rounded-lg bg-white text-sm font-normal text-slate-900 shadow-md ring-1 ring-slate-900/5 focus:outline-none sm:-mr-3.5 transform opacity-100 scale-100 z-50"
-											aria-labelledby="headlessui-menu-button-1"
-											id="headlessui-menu-items-122"
 											role="menu"
-											data-headlessui-state="open"
 										>
-											<div className="py-1.5" role="none">
-												<a
-													className="block py-1.5 block px-3.5 py-1.5 hover:bg-slate-100 text-gray-500"
-													id="headlessui-menu-item-123"
-													role="menuitem"
-													data-headlessui-state=""
-													href="https://docs.openpeer.xyz/"
-													target="_blank"
-												>
-													Docs
-												</a>
-												<a
-													className="block py-1.5 block px-3.5 py-1.5 hover:bg-slate-100 text-gray-500"
-													id="headlessui-menu-item-126"
-													role="menuitem"
-													data-headlessui-state=""
-													href="https://openpeer.xyz/disclamer"
-												>
-													Disclamer
-												</a>
-												<a
-													className="block py-1.5 block px-3.5 py-1.5 hover:bg-slate-100 text-gray-500"
-													id="headlessui-menu-item-126"
-													role="menuitem"
-													data-headlessui-state=""
-													href="https://openpeer.xyz/terms"
-												>
-													Terms
-												</a>
-												<a
-													className="block py-1.5 block px-3.5 py-1.5 hover:bg-slate-100 text-gray-500"
-													id="headlessui-menu-item-126"
-													role="menuitem"
-													data-headlessui-state=""
-													href="https://openpeer.xyz/privacy-policy"
-												>
-													Privacy Policy
-												</a>
+											<div className="py-1.5" role="menuItem">
+												{navigationItem.map(({ name, href }) => (
+													<Link
+														href={href}
+														className={`${
+															title === name
+																? 'border-indigo-500 text-gray-900'
+																: 'block py-1.5 block px-3.5 py-1.5 hover:bg-slate-100 text-gray-500'
+														} block py-1.5 block px-3.5 py-1.5 hover:bg-slate-100 text-gray-500`}
+														aria-current="page"
+														target="_blank"
+														key={name}
+													>
+														{name}
+													</Link>
+												))}
 											</div>
 											<div className="py-1.5" role="none">
 												<div className="flex flex-row py-1 px-4">
@@ -207,6 +182,50 @@ const NoAuthLayout = ({ Component, pageProps }: AppProps) => {
 									{name}
 								</Link>
 							))}
+							{navigationItem.map(({ name, href }) => (
+								<Link
+									href={href}
+									className={`${
+										title === name
+											? 'text-indigo-700'
+											: 'border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800'
+									} block border-l-4 py-2 pl-3 pr-4 text-base font-medium`}
+									aria-current="page"
+									key={name}
+								>
+									{name}
+								</Link>
+							))}
+							<div className="flex flex-row py-1 px-4">
+								<a
+									href="https://twitter.com/openpeer_xyz"
+									target="_blank"
+									rel="noreferrer"
+									className="px-2"
+								>
+									<Image
+										src={twitterLogo}
+										alt="openpeer logo"
+										className="opacity-40 hover:opacity-90"
+										width={20}
+										height={20}
+									/>
+								</a>
+								<a
+									href="https://discord.gg/Wrf9BT8sZN"
+									target="_blank"
+									rel="noreferrer"
+									className="px-2"
+								>
+									<Image
+										src={discord}
+										alt="openpeer logo"
+										className="opacity-40 hover:opacity-90"
+										width={21}
+										height={21}
+									/>
+								</a>
+							</div>
 						</div>
 					</div>
 				)}
