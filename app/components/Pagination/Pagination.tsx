93cc109db4608c565a7b26a58b63301365fcdc56
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-06-14 10:17:31 +0100

    Improve component

--- app/components/Pagination/Pagination.tsx
+++ app/components/Pagination/Pagination.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 interface PaginationProps {
 	next: string;
 	prev: string;
@@ -6,37 +8,32 @@ interface PaginationProps {
 	totalResult: string;
 }
 
-const Pagintion = ({ next, prev, minResult, maxResult, totalResult }: PaginationProps) => {
-	return (
-		<>
-			<nav
-				className="flex items-center justify-between border-t border-gray-200 px-4 py-8 sm:px-6 mt-8"
-				aria-label="Pagination"
+const Pagintion = ({ next, prev, minResult, maxResult, totalResult }: PaginationProps) => (
+	<nav
+		className="flex items-center justify-between border-t border-gray-200 px-4 py-8 sm:px-6 mt-8"
+		aria-label="Pagination"
+	>
+		<div className="hidden sm:block">
+			<p className="text-sm text-gray-700">
+				Showing <span className="font-medium">{minResult}</span> to
+				<span className="font-medium">{maxResult}</span> of
+				<span className="font-medium">{totalResult}</span> results
+			</p>
+		</div>
+		<div className="flex flex-1 justify-between sm:justify-end">
+			<a
+				href={prev}
+				className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
 			>
-				<div className="hidden sm:block">
-					<p className="text-sm text-gray-700">
-						Showing <span className="font-medium">{minResult}</span> to
-						<span className="font-medium">{maxResult}</span> of
-						<span className="font-medium">{totalResult}</span> results
-					</p>
-				</div>
-				<div className="flex flex-1 justify-between sm:justify-end">
-					<a
-						href={prev}
-						className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-					>
-						Previous
-					</a>
-					<a
-						href={next}
-						className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-					>
-						Next
-					</a>
-				</div>
-			</nav>
-		</>
-	);
-};
-
+				Previous
+			</a>
+			<a
+				href={next}
+				className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+			>
+				Next
+			</a>
+		</div>
+	</nav>
+);
 export default Pagintion;
