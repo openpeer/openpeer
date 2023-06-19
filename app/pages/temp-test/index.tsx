90b49e356a90ed2ec52b247c3628d30feefdcf14
avelar <fortunato.avelar@gmail.com>
2023-06-19 16:45:39 -0300

    contract text; decrease elegible tokens number font-size

--- app/pages/temp-test/index.tsx
+++ app/pages/temp-test/index.tsx
@@ -149,17 +149,17 @@ const AirdropCountdown = ({
 							<span className="font-light">{days >= 2 ? 'days' : 'day'}</span>
 						</div>
 					)}
-					<div className="flex items-center justify-around">
+					<div className="flex items-center justify-around space-x-2">
 						<span className="text-2xl md:text-3xl font-bold">{hours}</span>
 						<span className="font-light">{hours === 1 ? 'hour' : 'hours'}</span>
 					</div>
-					<div className="flex items-center justify-around">
-						<span className="text-2xl md:text-3xl font-bold">{minutes}</span>
-						<span className="font-light">{minutes === 1 ? 'minute' : 'minutes'}</span>
+					<div className="flex items-center justify-around space-x-2">
+						<span className="text-xl md:text-3xl font-bold">{minutes}</span>
+						<span className="font-light">{minutes === 1 ? 'min' : 'min'}</span>
 					</div>
-					<div className="flex items-center justify-around">
-						<span className="text-2xl md:text-3xl font-bold">{seconds}</span>
-						<span className="font-light">{seconds === 1 ? 'second' : 'seconds'}</span>
+					<div className="flex items-center justify-around space-x-2">
+						<span className="text-xl md:text-3xl font-bold">{seconds}</span>
+						<span className="font-light">{seconds === 1 ? 'sec' : 'sec'}</span>
 					</div>
 				</div>
 				<div className="my-4">
@@ -301,7 +301,7 @@ const AirdropPage = () => {
 								<div className="rounded-full mt-6 md:mt-0 p-[4px] bg-gradient-to-r from-[#6FD9EC] to-[#6BA4F8]">
 									<div className="flex flex-col w-[200px] h-[200px] md:w-[220px] md:h-[220px] p-10 justify-center items-center text-white bg-gradient-to-r from-[#2C76E5] to-[#6FD9EC] rounded-full p-8 text-gray-800">
 										<span className="text-white text-base mb-2">Eligible for</span>
-										<span className="text-white text-5xl font-bold mb-2">
+										<span className="text-white text-4xl font-bold mb-2">
 											{Number(tokens).toFixed(2)}
 										</span>
 										<span className="text-white text-2xl">vP2P</span>
