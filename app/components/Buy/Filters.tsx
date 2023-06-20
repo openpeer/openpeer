978cee29d603c83c1bd7ae58c2d4f1215aecbc7c
avelar <fortunato.avelar@gmail.com>
2023-06-20 08:48:41 -0300

    add select component

--- app/components/Buy/Filters.tsx
+++ app/components/Buy/Filters.tsx
@@ -4,33 +4,28 @@ import Select from 'components/Select/Select';
 import { FiatCurrency } from 'models/types';
 import React from 'react';
 
-const optionItems: {} = {
-	option1: 'option1',
-	option2: 'option2',
-	option3: 'option2'
-};
+const optionItems = [{ option1: 'option1', option2: 'option2', option3: 'option3' }];
 
 const Filters = () => (
 	<div className="flex items-center space-x-4">
 		<Input label="Search by amount" id="0" addOn="INR ₹" placeholder="Enter Amount" />
 		<Select
 			label="Verfication"
-			options={optionItems}
 			selected={undefined}
 			// eslint-disable-next-line react/jsx-no-bind, func-names
 			onSelect={function (): void {
 				throw new Error('Function not implemented.');
 			}}
+			options={[optionItems]}
 		/>
 		<Select
 			label="Payment Method"
-			options={optionItems}
-			updateValue={undefined}
 			selected={undefined}
 			// eslint-disable-next-line react/jsx-no-bind, func-names
 			onSelect={function (): void {
 				throw new Error('Function not implemented.');
 			}}
+			options={[optionItems]}
 		/>
 		<CurrencySelect
 			label="Currency"
