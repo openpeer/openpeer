a09e3906cfe9bdba0a4f6a548c122b0bbec0bc29
avelar <fortunato.avelar@gmail.com>
2023-07-01 17:03:19 -0300

    fix Trade P2P mobile table

--- app/components/ListsTable.tsx
+++ app/components/ListsTable.tsx
@@ -100,46 +100,60 @@ const ListsTable = ({ lists, fiatAmount, tokenAmount }: ListsTableProps) => {
 								<div className="w-full flex flex-row justify-around md:justify-start items-center">
 									<div className="w-3/5 mr-6">
 										<Link href={`/${sellerAddress}`}>
-											<div className="flex flex-col lg:flex-row lg:items-center cursor-pointer">
-												<div className="w-16 flex flex-row mb-2">
-													<Avatar user={seller} />
-												</div>
-												<div className="text-sm text-gray-900 text-ellipsis overflow-hidden">
+											<div className="flex flex-row items-center cursor-pointer">
+												<Avatar user={seller} className="w-5 md:w-8" />
+												<div className="pl-1 md:pl-2 text-sm text-gray-900 text-ellipsis overflow-hidden">
 													{name || smallWalletAddress(sellerAddress)}
 												</div>
 											</div>
 										</Link>
 										<div className="mt-1 flex flex-col text-gray-500 block lg:hidden">
-											<div className="flex flex-row items-center space-x-1">
-												<Token token={token} size={24} />
-												<span>
-													{amount} {symbol}
-												</span>
-												<Image
-													src={bank.icon}
-													alt={bank.name}
-													className="h-6 w-6 flex-shrink-0 rounded-full mr-1"
-													width={24}
-													height={24}
-													unoptimized
-												/>
-												<span>{bank.name}</span>
+											<div className="flex flex-col space-y-1">
+												<div className="flex flex-row items-center">
+													<div className="flex flex-col">
+														<div className="flex flex-row items-center">
+															<span className="pr-2 text-sm">Price</span>
+															<span>
+																<Flag name={countries[countryCode!]} size={20} />
+															</span>
+															<span className="-ml-2 border-2 border-white rounded-full">
+																<Token token={token} size={20} />
+															</span>
+														</div>
+														<span className="mb-2">
+															<div className="flex flex-row items-center">
+																<span className="pr-1 text-sm text-gray-800">
+																	{fiatSymbol} {Number(price).toFixed(2)} per
+																</span>
+																<span className="text-sm text-gray-800">{symbol}</span>
+															</div>
+														</span>
+													</div>
+												</div>
+												<div className="flex flex-row items-center">
+													<span className="pr-2 text-sm">Available</span>
+													<Token token={token} size={20} />
+												</div>
+												<div className="flex flex-row items-center">
+													<span className="mr-2 text-sm text-gray-800">
+														{amount} {symbol}
+													</span>
+												</div>
+												<div className="flex flex-row items-center">
+													<span className="pr-2 text-sm">{bank.name}</span>
+													<Image
+														src={bank.icon}
+														alt={bank.name}
+														className="h-6 w-6 flex-shrink-0 rounded-full mr-1"
+														width={24}
+														height={24}
+														unoptimized
+													/>
+												</div>
 											</div>
 										</div>
 									</div>
 									<div className="w-2/5 flex flex-col lg:hidden px-4">
-										<div className="flex flex-row items-center space-x-1 mb-2">
-											<Flag name={countries[countryCode!]} size={24} />
-											<span className="font-bold">
-												<div className="flex flex-row items-center space-x-1">
-													<span>
-														{fiatSymbol} {Number(price).toFixed(2)} per
-													</span>
-													<Token token={token} size={24} />
-													<span>{symbol}</span>
-												</div>
-											</span>
-										</div>
 										{isSeller ? (
 											<EditListButtons id={list.id} />
 										) : (
