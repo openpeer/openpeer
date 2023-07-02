7e90a8755053dfda8981ccef54d793c53c381d7a
avelar <fortunato.avelar@gmail.com>
2023-07-02 10:41:37 -0300

    update FeeDisplay from flex-col to flex-row

--- app/components/Buy/Payment.tsx
+++ app/components/Buy/Payment.tsx
@@ -62,25 +62,25 @@ const Payment = ({ order }: BuyStepProps) => {
 						</p>
 					</div>
 				)}
-				<div className="flex flex-row justify-around bg-gray-100 rounded-lg p-6 my-4">
-					<div className="flex flex-col">
-						<span className="text-sm">Amount to pay</span>
-						<span className="text-lg font-medium">
+				<div className="flex flex-col justify-around bg-gray-100 rounded-lg p-4 my-4">
+					<div className="flex flex-row items-center mb-1">
+						<span className="text-sm mr-2">Amount to pay</span>
+						<span className="text-base font-medium">
 							{selling
 								? `${Number(tokenAmount)?.toFixed(2)} ${token.symbol}`
 								: `${currency.symbol} ${Number(fiatAmount).toFixed(2)}`}
 						</span>
 					</div>
 					{selling && <FeeDisplay escrow={escrow?.address} token={token} tokenAmount={tokenAmount} />}
-					<div className="flex flex-col">
-						<span className="text-sm">Price</span>
-						<span className="text-lg font-medium">
+					<div className="flex flex-row items-center mb-1">
+						<span className="text-sm mr-2">Price</span>
+						<span className="text-base font-medium">
 							{currency.symbol} {Number(price).toFixed(2)}
 						</span>
 					</div>
-					<div className="flex flex-col">
-						<span className="text-sm">Amount to receive</span>
-						<span className="text-lg font-medium">
+					<div className="flex flex-row items-center mb-1">
+						<span className="text-sm mr-2">Amount to receive</span>
+						<span className="text-base font-medium">
 							{selling
 								? `${currency.symbol} ${Number(fiatAmount).toFixed(2)}`
 								: `${Number(tokenAmount)?.toFixed(2)} ${token.symbol}`}
