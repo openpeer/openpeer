93f3e37e62ca4f9825fb9d4181c0d59c476cd8d2
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-07-03 09:24:33 +0100

    Update contracts

--- app/hooks/transactions/escrow/useEscrowWithGas.ts
+++ app/hooks/transactions/escrow/useEscrowWithGas.ts
@@ -4,15 +4,16 @@ import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from
 
 import { UseEscrowFundsProps } from '../types';
 
-const useCreateContract = ({ orderID, buyer, amount, token, fee, contract }: UseEscrowFundsProps) => {
+const useEscrowWithGas = ({ orderID, buyer, amount, token, fee, contract }: UseEscrowFundsProps) => {
 	const { address } = token;
 	const nativeToken = address === constants.AddressZero;
+	const partner = constants.AddressZero;
 
 	const { config } = usePrepareContractWrite({
 		address: contract,
 		abi: OpenPeerEscrow,
 		functionName: nativeToken ? 'createNativeEscrow' : 'createERC20Escrow',
-		args: nativeToken ? [orderID, buyer, amount] : [orderID, buyer, address, amount],
+		args: nativeToken ? [orderID, buyer, amount, partner] : [orderID, buyer, address, amount, partner],
 		overrides: {
 			gasLimit: BigNumber.from('2000000'),
 			value: nativeToken ? amount.add(fee) : undefined
@@ -28,4 +29,4 @@ const useCreateContract = ({ orderID, buyer, amount, token, fee, contract }: Use
 	return { isLoading, isSuccess, escrowFunds: write, data, isFetching: false };
 };
 
-export default useCreateContract;
+export default useEscrowWithGas;
--- app/hooks/useEscrowFee.ts
+++ app/hooks/useEscrowFee.ts
@@ -1,9 +1,9 @@
 import { OpenPeerDeployer } from 'abis';
-import { BigNumber } from 'ethers';
+import { BigNumber, constants } from 'ethers';
 import { toBn } from 'evm-bn';
 import { DEPLOYER_CONTRACTS } from 'models/networks';
 import { Token } from 'models/types';
-import { useContractRead, useNetwork } from 'wagmi';
+import { useContractReads, useNetwork } from 'wagmi';
 import { polygon } from 'wagmi/chains';
 
 interface UseEscrowFeeParams {
@@ -16,19 +16,32 @@ const useEscrowFee = ({ address, tokenAmount, token }: UseEscrowFeeParams) => {
 	const { chain, chains } = useNetwork();
 	const deployer = DEPLOYER_CONTRACTS[(chain || chains[0] || polygon).id];
 	const contract = address || deployer;
+	const partner = constants.AddressZero;
 
-	const { data: feeBps, isFetching } = useContractRead({
-		address: contract,
-		abi: OpenPeerDeployer,
-		functionName: 'sellerFee'
+	const { data, isFetching } = useContractReads({
+		contracts: [
+			{
+				address: contract,
+				abi: OpenPeerDeployer,
+				functionName: 'sellerFee',
+				args: [partner]
+			},
+			{
+				address: contract,
+				abi: OpenPeerDeployer,
+				functionName: 'openPeerFee'
+			}
+		]
 	});
 
 	if (isFetching || !token || !tokenAmount) return { isFetching };
+
+	const [feeBps, openPeerFeeBps] = data as [BigNumber, BigNumber];
 
 	const rawTokenAmount = toBn(String(tokenAmount), token.decimals);
 	const fee = rawTokenAmount.mul(feeBps as BigNumber).div(BigNumber.from('10000'));
 	const totalAmount = rawTokenAmount.add(fee);
 
-	return { isFetching, fee, totalAmount };
+	return { isFetching, fee, partnerFeeBps: feeBps.sub(openPeerFeeBps), totalAmount };
 };
 export default useEscrowFee;
