ac58726a0ff9de1c540c9598289c26a5e35b0e38
avelar <fortunato.avelar@gmail.com>
2023-08-10 08:23:52 -0300

    improve partner logos position

--- site/components/Partners.tsx
+++ site/components/Partners.tsx
@@ -12,7 +12,7 @@ const Partners = () => {
 		<div className="w-full flex justify-center items-center flex-col text-center mb-16 md:mb-40">
 			<h3 className="text-5xl mb-12">Backers and Partners</h3>
 			<div className="w-full flex flex-col justify-center items-center md:flex-row gap-8">
-				<div className="grid grid-cols-3 gap-1 px-3 sm:grid-cols-4 md:px-0 lg:grid-cols-4 lg:gap-4">
+				<div className="grid grid-cols-3 gap-1 px-3 sm:grid-cols-5 md:px-0 lg:grid-cols-5 lg:gap-4">
 					<div className="flex items-center justify-center rounded-md border border-white/20 p-2 hover:bg-gradient-to-r from-[#010AD4]/50 to-transparent h-16">
 						<Image src={outlierVentures} alt="Outlier Ventures" />
 					</div>
@@ -20,10 +20,10 @@ const Partners = () => {
 						<Image src={polygonLogoWhite} alt="Polygon" />
 					</div>
 					<div className="flex items-center justify-center rounded-md border border-white/20 p-2 hover:bg-gradient-to-r from-[#010AD4]/50 to-transparent h-16">
-						<Image src={w3iLogoWhite} alt="Polygon" />
+						<Image className="w-20" src={w3iLogoWhite} alt="W3i" />
 					</div>
 					<div className="flex items-center justify-center rounded-md border border-white/20 p-2 hover:bg-gradient-to-r from-[#010AD4]/50 to-transparent] h-16">
-						<Image src={bitfwdLogoWhite} alt="Polygon" />
+						<Image src={bitfwdLogoWhite} alt="Bitfwd" />
 					</div>
 					<div className="flex items-center justify-center rounded-md border border-white/20 p-2 hover:bg-gradient-to-r from-[#010AD4]/50 to-transparent] h-16">
 						<Image src={ybbFoundationLtd} alt="YBB Foundation" />
