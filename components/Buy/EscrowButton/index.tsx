6bad2f96449521f9ded578644f8ae0730c36b7a7
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-09-21 16:02:58 +0100

    Add debug information

--- components/Buy/EscrowButton/index.tsx
+++ components/Buy/EscrowButton/index.tsx
@@ -16,8 +16,15 @@ const EscrowButton = ({ token, tokenAmount, buyer, uuid }: EscrowFundsParams) =>
 	const { chain } = useNetwork();
 	const deployer = DEPLOYER_CONTRACTS[chain!.id];
 	const { address } = useAccount();
+	console.log('DEPLOYER_CONTRACTS', DEPLOYER_CONTRACTS);
+	console.log('address', address);
+	console.log('deployer', deployer);
+	console.log('token', token);
+	console.log('tokenAmount', tokenAmount);
 
 	const { isFetching, fee, totalAmount } = useEscrowFee({ token, tokenAmount });
+	console.log('fee', fee);
+	console.log('totalAmount', totalAmount);
 	const { data: sellerContract } = useContractRead({
 		address: deployer,
 		abi: OpenPeerDeployer,
@@ -27,7 +34,10 @@ const EscrowButton = ({ token, tokenAmount, buyer, uuid }: EscrowFundsParams) =>
 		watch: true
 	});
 
+	console.log({ isFetching, sellerContract });
+
 	if (isFetching) return <></>;
+	console.log('sellerContract', sellerContract);
 
 	const needsToDeploy = !sellerContract || sellerContract === constants.AddressZero;
 
--- hooks/transactions/deploy/useDeploy.ts
+++ hooks/transactions/deploy/useDeploy.ts
@@ -6,15 +6,20 @@ import useGaslessDeploy from './useGaslessDeploy';
 
 const useDeploy = () => {
 	const { chain } = useNetwork();
+	console.log('Inside useDeploy.ts chain: ', chain);
 	const contract = DEPLOYER_CONTRACTS[chain?.id!];
+	console.log('Inside useDeploy.ts chain: ', contract);
 
 	const withGasCall = useDeployWithGas({
 		contract
 	});
+	console.log('Inside useDeploy.ts chain: ', withGasCall);
 
 	const { gaslessEnabled, isFetching, isLoading, isSuccess, data, deploy } = useGaslessDeploy({
 		contract
 	});
+
+	console.log('Inside useDeploy.ts gasless: ', { gaslessEnabled, isFetching, isLoading, isSuccess, data, deploy });
 
 	if (isFetching) {
 		return { isLoading: false, isSuccess: false, isFetching };
