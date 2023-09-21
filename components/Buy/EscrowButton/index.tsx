9655bdb2fec1b90de9f7867258d21df6d99dd568
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-09-21 16:30:03 +0100

    Revert "Add debug information"
    This reverts commit 6bad2f96449521f9ded578644f8ae0730c36b7a7.

--- components/Buy/EscrowButton/index.tsx
+++ components/Buy/EscrowButton/index.tsx
@@ -16,15 +16,8 @@ const EscrowButton = ({ token, tokenAmount, buyer, uuid }: EscrowFundsParams) =>
 	const { chain } = useNetwork();
 	const deployer = DEPLOYER_CONTRACTS[chain!.id];
 	const { address } = useAccount();
-	console.log('DEPLOYER_CONTRACTS', DEPLOYER_CONTRACTS);
-	console.log('address', address);
-	console.log('deployer', deployer);
-	console.log('token', token);
-	console.log('tokenAmount', tokenAmount);
 
 	const { isFetching, fee, totalAmount } = useEscrowFee({ token, tokenAmount });
-	console.log('fee', fee);
-	console.log('totalAmount', totalAmount);
 	const { data: sellerContract } = useContractRead({
 		address: deployer,
 		abi: OpenPeerDeployer,
@@ -34,10 +27,7 @@ const EscrowButton = ({ token, tokenAmount, buyer, uuid }: EscrowFundsParams) =>
 		watch: true
 	});
 
-	console.log({ isFetching, sellerContract });
-
 	if (isFetching) return <></>;
-	console.log('sellerContract', sellerContract);
 
 	const needsToDeploy = !sellerContract || sellerContract === constants.AddressZero;
 
