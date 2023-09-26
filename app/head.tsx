a5965b1a0c7855304457c7e3dad1ccc187e7cfe2
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-09-26 14:42:53 +0100

    Add clarity

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,27 @@
+/* eslint-disable no-param-reassign */
 import Head from 'next/head';
 import Script from 'next/script';
 import React from 'react';
 
 export default function Header() {
+	// @ts-expect-error
+	const clarity = (c, l, a, r, i, t, y) => {
+		c[a] =
+			c[a] ||
+			// eslint-disable-next-line func-names
+			function () {
+				// eslint-disable-next-line prefer-rest-params
+				(c[a].q = c[a].q || []).push(arguments);
+			};
+		t = l.createElement(r);
+		t.async = 1;
+		// eslint-disable-next-line prefer-template
+		t.src = 'https://www.clarity.ms/tag/' + i;
+		// eslint-disable-next-line prefer-destructuring
+		y = l.getElementsByTagName(r)[0];
+		y.parentNode.insertBefore(t, y);
+	};
+
 	return (
 		<>
 			<Head>
@@ -15,22 +34,26 @@ export default function Header() {
 				<link rel="icon" href="/favicon.png?v=2" />
 			</Head>
 			{process.env.NODE_ENV === 'production' && (
-				<Script
-					src="https://unpkg.com/@arcxmoney/analytics"
-					onLoad={() => {
-						// @ts-expect-error
-						ArcxAnalyticsSdk.init(
-							'7daa6d035cf0f0853f82c75a7377bb4a907989202c4cb93ee76532ddfb015445',
-							{}
-						).then(
+				<>
+					<Script
+						src="https://unpkg.com/@arcxmoney/analytics"
+						onLoad={() => {
 							// @ts-expect-error
-							(sdk) => {
+							ArcxAnalyticsSdk.init(
+								'7daa6d035cf0f0853f82c75a7377bb4a907989202c4cb93ee76532ddfb015445',
+								{}
+							).then(
 								// @ts-expect-error
-								window.arcx = sdk;
-							}
-						);
-					}}
-				/>
+								(sdk) => {
+									// @ts-expect-error
+									window.arcx = sdk;
+								}
+							);
+						}}
+					/>
+					{/* @ts-expect-error */}
+					<Script id="clarity">{clarity(window, document, 'clarity', 'script', 'j17nowbio9')}</Script>
+				</>
 			)}
 		</>
 	);
