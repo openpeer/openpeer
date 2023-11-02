2b45f1b1bbbdd22c5ebfb318687a4254bc09087f
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-11-02 17:09:31 +0000

    Fix allowance bug

--- components/Buy/EscrowButton/ApproveTokenButton.tsx
+++ components/Buy/EscrowButton/ApproveTokenButton.tsx
@@ -44,7 +44,7 @@ const ApproveTokenButton = ({
 		args: [address!, spender]
 	});
 
-	const approved = (allowance || BigInt(0)) >= amount;
+	const approved = !!allowance && !!amount && allowance >= amount;
 
 	useEffect(() => {
 		if (isSuccess || approved) onTokenApproved();
