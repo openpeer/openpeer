b82bfc4861103fbda72cde3a515afa0944b82a88
Marcos Teixeira <m.viniteixeira@gmail.com>
2024-01-01 10:23:41 +0000

    Bump airdrop version

--- pages/airdrop.tsx
+++ pages/airdrop.tsx
@@ -12,9 +12,9 @@ import { DynamicWidget, getAuthToken } from '@dynamic-labs/sdk-react-core';
 import { constants } from 'ethers';
 import { useAccount } from 'hooks';
 
-const ROUND = 7;
+const ROUND = 8;
 const POOL = 1000000;
-const AIRDROP_START = 1704110400000;
+const AIRDROP_START = 1706788800000;
 
 const CallToActionButton = ({ address }: { address: `0x${string}` | undefined }) => {
 	const router = useRouter();
